b4c9b5917e2c877dbeca94c0ef56c8e99d71312a
Sengirs <yannick.petit@gmail.com>
2012-11-22 09:23:00 +0100

    commit initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mpimmo::Application.config.secret_token = 'e76c09cc7b1d5d16efadd08997e168c08bcf9901f29310eb4969c5033f8cda205b832500ccca40a59cf452f2fb46a48761e4d240486b368b06844069ad2a9b1c'
